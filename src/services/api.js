6c804c3858d9f0354ee4560d93277f2fd7b7716a
ShaneCoelho <shanecoelho1@gmail.com>
2025-12-08 00:01:17 +0530

    updated the backend url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:4000';
+const API_BASE_URL = 'https://fynd-ecommerce-backend.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
